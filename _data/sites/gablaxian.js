c51ea260a579fcbb26e6c8232b203cfb006e39f7
Graham Smith <g@blaxian.com>
2020-07-13 15:42:17 +0100

    fix: adds personal site. removes other tests.

---
+++ _data/sites/gablaxian.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "gablaxian.com", // optional, falls back to object key
+  description: "Personal web site",
+  options: {
+    frequency: 60 * 23, // 23 hours
+    freshChrome: "site",
+  },
+  urls: [
+    "https://gablaxian.com/",
+    "https://gablaxian.com/articles",
+    "https://gablaxian.com/experiments",
+    "https://gablaxian.com/blog",
+  ],
+};
